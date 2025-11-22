0c4bd706dbdcc401f1c7fb000a85ef6e01b96ffe
Anish <anish27863@gmail.com>
2025-11-22 19:14:54 +0530

    creating a function, read_apikey() to read the api key data from the file. and improving the overall structure of the code

--- weather_data.py
+++ weather_data.py
@@ -1,33 +1,31 @@
 import requests
-f=open("apikey.txt","r")
-ak=f.read()
-ak=ak[11:len(ak)-1]
+
+
+def read_apikey():
+    '''Function to read API key from file'''
+    with open("apikey.txt", "r") as f:
+        return f.read().strip()[11:-1]
+
 class WeatherData:
-    def __init__(self, api_key):
+    def __init__(self, api_key=None):
+        if not api_key:
+            api_key = read_apikey()
         if not api_key:
             raise ValueError("API key required")
         self.api_key = api_key
         self.main = {}
         self.weather_desc = ""
-    
-    def get_weather(city):
-        api_key = ak 
+        self.data = {}
+
+    def get_weather(self, city):
         base_url = "https://api.openweathermap.org/data/2.5/weather"
-        params = {'q': city, 'appid': api_key, 'units': 'metric'}
+        params = {'q': city, 'appid': self.api_key, 'units': 'metric'}
         response = requests.get(base_url, params=params)
 
         if response.status_code == 200:
-            data = response.json()
-            main = data['main']
-            weather_desc = data['weather'][0]['description']
-            print(f"Weather in {city}: {weather_desc}")
-            print(f"Temperature: {main['temp']}°C")
-            print(f"Humidity: {main['humidity']}%")
-            print(main)
-            
+            self.data = response.json()
+            self.main = self.data['main']
+            self.weather_desc = self.data['weather'][0]['description']
+            return self.data
         else:
-            print(f"City {city} not found. Please check the name.")
-
-
-    # city = input("Enter city name: ")
-    # get_weather(city)
+            return None
